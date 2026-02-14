4caac53b2312621fd8644e91e409bc6a67ef0f04
Jasmine Tai <jtai@jtai.ca>
2026-02-13 19:13:05 -0500

    Add basic canvas rendering and state management

---
+++ src/lib/models.ts
@@ -0,0 +1,84 @@
+export type Edge = [number, number];
+
+/// A 2D vector.
+export class Vec2 {
+	public static readonly ZERO = new Vec2(0, 0);
+
+	/**
+	 * Constructs a new instance of the class with the given x and y coordinates.
+	 *
+	 * @param x - The x-coordinate.
+	 * @param y - The y-coordinate.
+	 */
+	constructor(
+		public readonly x: number,
+		public readonly y: number,
+	) {}
+
+	/**
+	 * Adds the components of another Vec2 object to this Vec2 object.
+	 *
+	 * @param other - The vector to be added.
+	 * @return A new Vec2 object representing the sum of the two vectors.
+	 */
+	public add(other: Vec2): Vec2 {
+		return new Vec2(this.x + other.x, this.y + other.y);
+	}
+
+	/**
+	 * Subtracts the components of the given vector from the current vector.
+	 *
+	 * @param other - The vector to subtract from the current vector.
+	 * @return A new vector representing the result of the subtraction.
+	 */
+	public sub(other: Vec2): Vec2 {
+		return new Vec2(this.x - other.x, this.y - other.y);
+	}
+
+	/**
+	 * Scales the vector by the given factor.
+	 *
+	 * @param factor - The factor by which to scale the vector.
+	 * @return A new vector that is the result of scaling the original vector by the factor.
+	 */
+	public scale(factor: number): Vec2 {
+		return new Vec2(this.x * factor, this.y * factor);
+	}
+
+	/**
+	 * Calculates the length (magnitude) of a vector defined by its x and y components.
+	 *
+	 * @return The length of the vector.
+	 */
+	public length(): number {
+		return Math.hypot(this.x, this.y);
+	}
+
+	/**
+	 * Normalizes the vector, scaling it to have a magnitude of 1 while maintaining its direction.
+	 * If the vector's length is zero, this returns the zero vector.
+	 *
+	 * @return A new vector instance with a magnitude of 1 in the same direction as the original vector.
+	 */
+	public normalize(): Vec2 {
+		const len = this.length();
+		if (len === 0) return Vec2.ZERO;
+		return this.scale(1 / len);
+	}
+}
+
+/// The singleton game state.
+export const State = {
+	/// The data that is persisted to the save file.
+	save: {
+		money: 0,
+		dependencyGraph: [] as Edge[],
+		obtainedUpgrades: [] as boolean[],
+	},
+
+	playerPosition: Vec2.ZERO,
+	facingDirection: new Vec2(1, 0),
+};
+
+/// Data that is persisted to the save file.
+export type SaveData = typeof State.save;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import {State} from "$lib/models";
+    import {onMount} from "svelte";
+
+    let canvas: HTMLCanvasElement;
+
+    const cameraScale = 4;
+
+    function render() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Set up camera
+        ctx.resetTransform();
+        ctx.translate(500, 500);
+        ctx.scale(cameraScale, -cameraScale);
+
+        ctx.fillStyle = 'red';
+        ctx.ellipse(State.playerPosition.x, State.playerPosition.y, 10, 10, 0, 0, 2 * Math.PI);
+
+        ctx.fill();
+    }
+
+    function onKey(event: KeyboardEvent) {
+
+    }
+
+    function runAnimationFrame() {
+        render();
+        requestAnimationFrame(runAnimationFrame);
+    }
+
+    onMount(runAnimationFrame);
+</script>
+
+<svelte:body on:keydown={onKey}/>
+
+<canvas bind:this={canvas} width={1000} height={1000}></canvas>
+
+<style>
+    canvas {
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+    }
+</style>
